fd9f8c2dbf71a23890e9d69a64326382a41d2042
AGaleano97 <52254706+AGaleano97@users.noreply.github.com>
2019-07-18 21:53:46 -0400

    Added Score and Lives texts

--- Platformer/TileMap/Assets/Scripts/PlayerController.cs
+++ Platformer/TileMap/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
     public float speed;
-    
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
     public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText ();
+        SetLivesText ();
     }
 
     // Update is called once per frame
@@ -41,6 +53,40 @@ public class PlayerController : MonoBehaviour
             {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+        if (other.gameObject.CompareTag ("Enemy"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetLivesText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Score: " + count.ToString ();
+        if (count >= 4)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            loseText.text = "You Lose!";
         }
     }
 }
